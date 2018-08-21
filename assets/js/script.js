cf89f16b452e8d0381c6a1d6f7663d3f6b82a86a
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2018-08-20 23:19:13 -0400

    layout

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+heroOne = {
+    name:"Hero one",
+    weapon:"",
+    HP:"",
+    attackPower:"",
+    CounterAp:"",
+}
+heroTwo = {
+    name:"Hero Two",
+    weapon:"",
+    HP:"",
+    attackPower:"",
+    CounterAp:"",
+}
+heroThree = {
+    name:"Hero Three",
+    weapon:"",
+    HP:"",
+    attackPower:"",
+    CounterAp:"",
+}
+heroFour = {
+    name:"Hero Four",
+    weapon:"",
+    HP:"",
+    attackPower:"",
+    CounterAp:"",
+}
+
+
