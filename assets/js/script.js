90f05859077d7bb0f2d880b8eeb6582e5bb8c4e3
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2018-08-23 13:27:49 -0400

    whatever I did, I dont know

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,86 @@
-heroOne = {
-    name:"Hero one",
-    weapon:"",
-    HP:"",
-    attackPower:"",
-    CounterAp:"",
-}
-heroTwo = {
-    name:"Hero Two",
-    weapon:"",
-    HP:"",
-    attackPower:"",
-    CounterAp:"",
-}
-heroThree = {
-    name:"Hero Three",
-    weapon:"",
-    HP:"",
-    attackPower:"",
-    CounterAp:"",
-}
-heroFour = {
-    name:"Hero Four",
-    weapon:"",
-    HP:"",
-    attackPower:"",
-    CounterAp:"",
-}
+heros = [];
+var userChoice;
 
+var heros = {
+    "heroOne": {
+        name: "Hero one",
+        weapon: "Laser",
+        HP: "115",
+        attackPower: "10",
+        CounterAp: "9",
+    },
+    "heroTwo": {
+        name: "Hero Two",
+        weapon: "Mits",
+        HP: "200",
+        attackPower: "6",
+        CounterAp: "5",
+    },
+    "heroThree": {
+        name: "Hero Three",
+        weapon: "Sword",
+        HP: "155",
+        attackPower: "8",
+        CounterAp: "6",
+    },
+    "heroFour":{
+        name: "Hero Four",
+        weapon: "Chain",
+        HP: "130",
+        attackPower: "7",
+        CounterAp: "8",
+    }}
+
+    var userChoice;
+    var chosenEnemy;
+    var enemyGallery = [];
+
+   
+
+// start some business
+    $(document).ready(function () {
+// set hero HP
+        $("#heroOneHp").append(heroOne.HP);
+        $("#heroTwoHp").append(heroTwo.HP);
+        $("#heroThreeHp").append(heroThree.HP);
+        $("#heroFourHp").append(heroFour.HP);
+
+
+// create click event in which user chooses one object from an array
+userChoice =
+    $("#userChoices").click(function (heros) {
+        console.log(userChoice);
+        // userChoice.hide;
+        // if (userChoice === heroOne) {
+        //     alert("something");
+        
+// )))))))))))))))))))))))))))) DOT HIDE ((((((((((((((((((((((((()))))))))))))))))))))))))
+//     var userChoice;
+//     var userHero;
+
+// userChoice =
+//     $(".playerCard").click(function () {
+//        console.log(userChoice); 
+        // if (userChoice === heros.indexof(2)) {
+        //     console.log("picked");
+        //     $("#heroTwo #heroThree #heroFour").attr("visibilty", "hidden");
+            // } else if (userChoice === heros.indexOf(1)) {
+            //     $("#heroOne #heroThree #heroFour").attr("visibilty", "hidden");
+            // } else if (userChoice === heros.indexOf(2)) {
+            //     $("#heroOne #heroTwo #heroFour").attr("visibilty", "hidden");
+            // } else if (userChoice === heros.indexOf(3)) {
+            //     $("#heroOne #heroTwo #heroThree").attr("visibilty", "hidden");
+            // } 
+
+
+            // });
+    
+    });
+// });
+
+
+
+
+
+    });
 
