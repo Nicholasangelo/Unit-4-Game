0fbb00e156391e961150c6eb9d70c644278667fa
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2018-08-24 13:57:21 -0400

    meh, I can choose but not specific

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,40 @@
 heros = [];
 var userChoice;
 
-var heros = {
-    "heroOne": {
+var heros = [
+    heroOne = {
         name: "Hero one",
         weapon: "Laser",
         HP: "115",
         attackPower: "10",
         CounterAp: "9",
     },
-    "heroTwo": {
+    heroTwo = {
         name: "Hero Two",
         weapon: "Mits",
         HP: "200",
         attackPower: "6",
         CounterAp: "5",
     },
-    "heroThree": {
+    heroThree = {
         name: "Hero Three",
         weapon: "Sword",
         HP: "155",
         attackPower: "8",
         CounterAp: "6",
     },
-    "heroFour":{
+    heroFour = {
         name: "Hero Four",
         weapon: "Chain",
         HP: "130",
         attackPower: "7",
         CounterAp: "8",
-    }}
+    }]
 
     var userChoice;
     var chosenEnemy;
     var enemyGallery = [];
 
-   
 
 // start some business
     $(document).ready(function () {
@@ -45,35 +44,12 @@ var heros = {
         $("#heroThreeHp").append(heroThree.HP);
         $("#heroFourHp").append(heroFour.HP);
 
-
-// create click event in which user chooses one object from an array
-userChoice =
-    $("#userChoices").click(function (heros) {
-        console.log(userChoice);
-        // userChoice.hide;
-        // if (userChoice === heroOne) {
-        //     alert("something");
         
-// )))))))))))))))))))))))))))) DOT HIDE ((((((((((((((((((((((((()))))))))))))))))))))))))
-//     var userChoice;
-//     var userHero;
-
-// userChoice =
-//     $(".playerCard").click(function () {
-//        console.log(userChoice); 
-        // if (userChoice === heros.indexof(2)) {
-        //     console.log("picked");
-        //     $("#heroTwo #heroThree #heroFour").attr("visibilty", "hidden");
-            // } else if (userChoice === heros.indexOf(1)) {
-            //     $("#heroOne #heroThree #heroFour").attr("visibilty", "hidden");
-            // } else if (userChoice === heros.indexOf(2)) {
-            //     $("#heroOne #heroTwo #heroFour").attr("visibilty", "hidden");
-            // } else if (userChoice === heros.indexOf(3)) {
-            //     $("#heroOne #heroTwo #heroThree").attr("visibilty", "hidden");
-            // } 
-
-
-            // });
+// create click event in which user chooses one object from an array
+    $(".playerCard").click(function () {
+        userChoice=this;
+        console.log(userChoice);
+        
     
     });
 // });
